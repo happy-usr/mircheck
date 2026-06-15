838c0ebeadf9235a1e4f2ea24eea6bd78d25b162
sahand <brainyprogrammerforexx@gmail.com>
2026-06-15 23:32:21 +0330

    feat: add CmpDriverWithErrSqliteConstraint(). it checks whether an err derives from a particular type

--- internal/common/common.go
+++ internal/common/common.go
@@ -2,7 +2,7 @@ package common
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	sqlite3 "github.com/mattn/go-sqlite3"
 	"errors"
 )
 
@@ -25,5 +25,22 @@ func Open(db DBSelection) (*sql.DB, error) {
 			return nil, ErrInvalidDBSelection
 	}
 	return sql.Open("sqlite3", dbAddr)
+}
+
+func CmpDriverWithErrSqliteConstraint(err error, 
+	sqliteConstraint sqlite3.ErrNoExtended) bool {
+	if err == nil && sqliteConstraint == 0 {
+		return true
+	}
+	var tmpSqliteErr sqlite3.Error
+	cmp := errors.As(err, &tmpSqliteErr)
+	if !cmp {
+		return false
+	}
+	if tmpSqliteErr.Code != sqlite3.ErrConstraint ||
+		tmpSqliteErr.ExtendedCode != sqliteConstraint {
+		return false
+	}
+	return true
 }
 
--- internal/common/common_test.go
+++ internal/common/common_test.go
@@ -3,6 +3,7 @@ package common
 import (
 	"testing"
 	"errors"
+	sqlite3 "github.com/mattn/go-sqlite3"
 )
 
 func TestOpen(t *testing.T) {
@@ -49,3 +50,92 @@ func TestOpen(t *testing.T) {
 		})
 	}
 }
+
+type TestCmpDriverErrSqlConstStruct struct {
+	err error
+	sqlConstraint sqlite3.ErrNoExtended
+        testName string
+	expectedOutput bool
+}
+
+type tmpStruct struct {
+
+}
+func (ts tmpStruct) Error() string {
+	return ""
+}
+
+func makeAndAddErrs(tests *[]TestCmpDriverErrSqlConstStruct) {
+	sqlite3Errs := []sqlite3.Error {
+		{Code: sqlite3.ErrAuth /*anything other than ErrConstraint*/,
+			ExtendedCode: sqlite3.ErrConstraintRowID},
+		{Code: sqlite3.ErrConstraint, ExtendedCode: 
+			sqlite3.ErrConstraintCheck},
+		{Code: sqlite3.ErrConstraint, ExtendedCode:
+			sqlite3.ErrConstraintNotNull},
+		{Code: sqlite3.ErrConstraint, ExtendedCode:
+			sqlite3.ErrConstraintPrimaryKey},
+		{Code: sqlite3.ErrConstraint, ExtendedCode:
+			sqlite3.ErrConstraintUnique},
+		{Code: sqlite3.ErrConstraint, ExtendedCode:
+			sqlite3.ErrConstraintRowID},
+	}
+	var tmpTest TestCmpDriverErrSqlConstStruct 
+	tmpTest.err = tmpStruct{} /*anything other than expected error*/
+	tmpTest.sqlConstraint = sqlite3Errs[0].ExtendedCode
+	tmpTest.testName = "unexpected error type"
+	tmpTest.expectedOutput = false
+	*tests = append(*tests, tmpTest)
+
+	tmpTest.err = sqlite3Errs[0]
+	tmpTest.sqlConstraint = sqlite3Errs[0].ExtendedCode
+	tmpTest.testName = "unexpected error code"
+	tmpTest.expectedOutput = false
+	*tests = append(*tests, tmpTest)
+
+	tmpTest.err = sqlite3Errs[1]
+	tmpTest.sqlConstraint = sqlite3Errs[1].ExtendedCode
+	tmpTest.testName = "check constraint"
+	tmpTest.expectedOutput = true
+	*tests = append(*tests, tmpTest)
+	
+	tmpTest.err = sqlite3Errs[2]
+	tmpTest.sqlConstraint = sqlite3Errs[2].ExtendedCode
+	tmpTest.testName = "NotNull constraint"
+	tmpTest.expectedOutput = true
+	*tests = append(*tests, tmpTest)
+	
+	tmpTest.err = sqlite3Errs[3]
+	tmpTest.sqlConstraint = sqlite3Errs[4].ExtendedCode
+	tmpTest.testName = "PK constraint"
+	tmpTest.expectedOutput = false
+	*tests = append(*tests, tmpTest)
+	
+	tmpTest.err = sqlite3Errs[4]
+	tmpTest.sqlConstraint = sqlite3Errs[5].ExtendedCode
+	tmpTest.testName = "unique constraint"
+	tmpTest.expectedOutput = false
+	*tests = append(*tests, tmpTest)
+}
+
+func TestCmpDriverErrWithSqliteConstraint(t *testing.T) {
+	tests := []TestCmpDriverErrSqlConstStruct {
+		{nil, 0, "nil error 1", true},
+		{nil, sqlite3.ErrIoErrRead /*anything other than ErrConstraints*/, "nil error 2", false},
+	}
+	makeAndAddErrs(&tests)
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			t.Parallel()
+			got := CmpDriverWithErrSqliteConstraint(test.err,
+				test.sqlConstraint)
+			if test.expectedOutput != got {
+				t.Fatalf("%s: expected %t, got %t\n",
+				test.testName, test.expectedOutput,
+				got)
+			}
+		})
+	}
+}
+
+
