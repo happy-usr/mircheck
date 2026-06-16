c8c6c34dfd5c9ec4d810d9a5281dcf0d42b3861e
sahand <brainyprogrammerforexx@gmail.com>
2026-06-16 23:53:22 +0330

    feat: add AddResource(). used to add a new resource and its type

---
+++ internal/resources/resources.go
@@ -0,0 +1,19 @@
+package resources
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Resource struct {
+	DB       *sql.DB
+	Type     string
+	Resource string
+}
+
+func AddResource(resource Resource) error {
+	query := fmt.Sprintf("INSERT INTO resources VALUES(%q, %q)",
+		resource.Type, resource.Resource)
+	_, err := resource.DB.Exec(query)
+	return err
+}
---
+++ internal/resources/resources_test.go
@@ -0,0 +1,65 @@
+package resources
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/happy-usr/mircheck/internal/common"
+	sqlite3 "github.com/mattn/go-sqlite3"
+	"github.com/xyproto/randomstring"
+	"testing"
+)
+
+type TestResource struct {
+	resource    Resource
+	testName    string
+	wantErr     bool
+	expectedErr sqlite3.ErrNoExtended
+}
+
+var gType, gResource string
+var gDB *sql.DB
+
+func init() {
+	gType, gResource = genRandomString(), genRandomString()
+	gDB, _ = common.Open(common.TEST)
+}
+
+func genRandomString() string {
+	return randomstring.HumanFriendlyEnglishString(10)
+}
+
+func diagErr(err error) string {
+	var got_val string
+	if err == nil {
+		got_val = "(error is nil)"
+	} else {
+		got_val = fmt.Sprintf("error: %q", err.Error())
+	}
+	return got_val
+}
+
+func TestAddResource(t *testing.T) {
+	tests := []TestResource{
+		{Resource{gDB, gType, gResource}, "simple test", false, 0},
+		{Resource{gDB, gType, gResource}, "PK duplication", true, sqlite3.ErrConstraintPrimaryKey},
+		{Resource{gDB, gType, ""}, "null PK", true, sqlite3.ErrConstraintCheck},
+		{Resource{gDB, "", genRandomString()}, "null 'type' column", true, sqlite3.ErrConstraintCheck},
+	}
+	defer gDB.Close()
+
+	for _, test := range tests {
+		err := AddResource(test.resource)
+		if test.wantErr != (err != nil) {
+			got_val := diagErr(err)
+			t.Fatalf("%s: unexpected error value; %s\n",
+				test.testName, got_val)
+		}
+		if test.wantErr {
+			if !common.CmpDriverWithErrSqliteConstraint(err, test.expectedErr) {
+				t.Fatalf("%s: unexpected error type; expected "+
+					"%q, got %q\n", test.testName,
+					test.expectedErr.Error(), err.Error())
+			}
+		}
+	}
+}
